704bd3610941e301e1968f91660c50d6751a2da3
PyMarcus <marcus-v@outlook.com>
2022-06-02 22:58:13 -0300

    sub inteiros

---
+++ sub.asm
@@ -0,0 +1,6 @@
+.text 
+
+
+li $8, 15  # carrega, imediatamente, o valor 5 para o endereço $8
+li $9, 6  # carrega, imediatamente, o valor 6 para o endereço $9
+sub $10, $9, $8 # subtrai os valores contidos nos registradores $9 e $8 
