8e99ebf4187ec5a5d27518d93755a95b018a1724
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-07-14 06:51:40 -0300

    Add files via upload

---
+++ vetor.asm
@@ -0,0 +1,25 @@
+# percorrer o vetor
+
+.data 
+
+
+	vetor: .word 1, 2, 3, 4, 5  # 32 bites (8 bytes)
+
+.text 
+	# apontando para a base do vetor
+	la $t0, vetor 
+	
+	# setando  o print:
+	li $v0, 1
+	li $t1, 20  # limite de tamanho
+	li $t2, 0   # indice do vetor
+	
+	loop:
+		lw $a0, ($t0)  # carrega 32 bits para a0 no endereco de memoria de t0(aponta) #pega o valor que está no endereço de memória
+		syscall # imprime na tela
+		
+		addi $t2, $t2, 4  # 4 bits ( de 4 em 4 bits, percorre-se o vetor)
+		addi $t0, $t0, 4  # acrescenta de 4 em 4
+	blt $t2, $t1, loop 
+
+	
